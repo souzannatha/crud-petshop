81877946fa33a58d251fc6e5e88ad162ef244f9b
souzannatha <souzanatha04@gmail.com>
2025-05-16 20:11:32 -0300

    feat: added conection with bd.

---
+++ src/database/database.providers.ts
@@ -0,0 +1,24 @@
+
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'root',
+        password: 'root',
+        database: 'petshop',
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
